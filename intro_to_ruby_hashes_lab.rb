2a085f7c94d7101adde2f71880d5ce2d500169dd
Alex Rodriguez <alexrod102797@gmail.com>
2019-08-08 00:53:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,7 @@ def update_counting_hash(hash, key)
   if hash[key] 
     hash[key] += 1 
   else 
-    hash[key] = 1 
+    hash[key] = key=>1 
   end
 end
   # given a hash an a key as parameters, return an updated hash
